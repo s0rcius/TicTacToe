91411d8a3ad89cdb246b576a40e1406e81fa5bcf
Vincent1vp <vincent1vp@mail.com>
2018-09-10 10:00:49 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Init(args);
+    }
+
+    public static void Init(String[] args) {
+        System.out.println("Hi\r\n");
+    }
+}
