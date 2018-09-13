697d588e492fadb6b9639b83056e25fd489df872
Vincent1vp <vincent1vp@mail.com>
2018-09-13 12:12:16 +0200

    #1

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,49 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        Init(args);
+//        System.out.println("Hello World!");
+//        init();
+        new Main().init();
     }
 
-    public static void Init(String[] args) {
-        System.out.println("Hi\r\n");
+
+    private int playersTurn = 0; // 0 = Player 1, 1 = Player 2
+    private int[][] tttField = new int[3][3];
+
+    /**
+     * Initialize
+     *
+     */
+    public void init() {
+        playersTurn = 0;
+        tttField = new int[3][3];
+
+        System.out.println("Success");
+
+//        tttField[0][0] = 5;
+//        tttField[2][2] = 2;
+
+        System.out.println(tttField[0][0]);
+        System.out.println(tttField[2][2]);
+        for (int i = 0; i < tttField.length; i++) {
+            for (int j = 0; j < tttField.length; j++) {
+                System.out.println(tttField[i][j]);
+            }
+        }
+    }
+
+
+    public void test() {
+        //string s = "b";
+//        for (int i = 0; i < 5; i++) {
+//            System.out.println(i + 1);
+//            for (int j = 0; j < 5; j++) {
+//                System.out.println(i + 1);
+//            }
+//        }
+
+//        Scanner sc = new Scanner(System.in);
+//        int input = sc.nextInt();
+//        System.out.println(input);
     }
 }
